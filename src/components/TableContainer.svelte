feb3a085e644a5d5e26923e5cfc78d732b9e8081
morad98 <morad98@gmail.com>
2022-06-05 00:30:51 +0300

    Pages and Components are working

---
+++ src/components/TableContainer.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Table from './Table.svelte'
+    import Tablefilter from './TableFilter.svelte'
+</script>
+<h1>Table Container</h1>
+
+<Table/>
+<Tablefilter/>
+
--- src/routes/[state].svelte
+++ src/routes/[state].svelte
@@ -1,12 +1,20 @@
+<script context="module">
+    export async function preload(page) {
+        
+        return {state: page.params['state']};
+
+    }
+</script>
+
 <script>
     import CovidState from "../components/CovidState.svelte";
     import CovidChart from "../components/CovidChart.svelte";
     import TableContainer from "../components/TableContainer.svelte";
+    export let state;
 
 </script>
 
 <CovidState/>
 <CovidChart/>
-<TableContainer/>
 
-
+<h1>{state}</h1>
